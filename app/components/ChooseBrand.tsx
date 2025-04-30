c4d140db8bd7e5d535713fdc3d356fd97b8a6136
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2025-04-30 23:16:56 +0400

    lesson_2

--- app/components/ChooseBrand.tsx
+++ app/components/ChooseBrand.tsx
@@ -2,8 +2,12 @@
 
 import Link from 'next/link';
 import { cars } from '../services/cars';
+import { useState } from 'react';
+import { animate, motion } from 'framer-motion';
 
 const ChooseBrand = () => {
+  const [brand, setBrand] = useState('');
+
   return (
     <div>
       <h2 className='font-bold text-[20px] border-b-[1px] border-[black]'>
@@ -12,11 +16,25 @@ const ChooseBrand = () => {
       <div>
         <ul className='py-[5px]'>
           {cars.map((item) => (
-            <Link key={item.car} href={`/base/${item.car}`}>
-              <li className='py-[5px] font-medium'>{item.car}</li>
-            </Link>
+            <li
+              key={item.car}
+              className='py-[5px] font-medium cursor-pointer'
+              onClick={() => setBrand(item.car)}
+            >
+              {item.car}
+            </li>
           ))}
         </ul>
+
+        {brand && (
+          <motion.div initial={{ x: '-100vw' }} animate={{ x: 0 }}>
+            <Link href={`base/${brand}`}>
+              <button className='w-fit p-[10px] mt-[20px]  rounded-4xl bg-[#0c158d] cursor-pointer'>
+                see all cars
+              </button>
+            </Link>
+          </motion.div>
+        )}
       </div>
     </div>
   );
