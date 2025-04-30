cb0e51262732f94b8dc117cd2b9491309911e199
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2025-04-30 21:17:16 +0400

    core/structure

---
+++ app/components/Header.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+const header = () => {
+  return (
+    <header className='w-full flex justify-center items-center'>
+      <div className='max-w-[1280px] w-full flex justify-between'>
+        <div>LOGO</div>
+      </div>
+    </header>
+  );
+};
+
+export default header;
