43d1f0ea56371ff6de03dfc10df9ccd307c11388
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2025-04-30 21:37:00 +0400

    f

---
+++ app/(viwes)/Base/page.tsx
@@ -0,0 +1,11 @@
+export default function page() {
+  return (
+    <div className='w-full h-[100dvh] flex justify-center items-center'>
+      <div>
+        <h2 className='font-bold text-[20px] border-b-[1px] border-[black]'>
+          Choose Your Base
+        </h2>
+      </div>
+    </div>
+  );
+}
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,13 +1,15 @@
 'use client';
 
-const header = () => {
+const Header = () => {
   return (
     <header className='w-full flex justify-center items-center'>
-      <div className='max-w-[1280px] w-full flex justify-between'>
-        <div>LOGO</div>
+      <div className='max-w-[1280px] w-full flex justify-between border-b-[2px] border-[black] py-[20px]'>
+        <div>
+          <span className='text-[25px]'>LOGO</span>
+        </div>
       </div>
     </header>
   );
 };
 
-export default header;
+export default Header;
